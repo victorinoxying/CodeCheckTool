2533b51cc78d2fb130d7b04bbe764ca1e5461577
victorinox <15332434616@163.com>
2018-12-24 13:06:00 +0800

    finish

--- generator/generate.py
+++ generator/generate.py
@@ -1,3 +1,4 @@
+import os
 import random
 from random import choice
 from generator.SymbolTable import Symbol,SymbolTable
@@ -12,13 +13,14 @@ MAX_OBJECT_NUM = 10
 MAX_VALUE = 100
 TYPE_INT = 'int'
 TYPE_UINT = 'unsigned int'
+
 def standardizeStr(code, tag, level):
     length = len(code)
     result = level*'\t'+code+ (MAXLENTH-length-level*4)*' '+tag+'\n'
     return result
 
 HEAD = standardizeStr('#include <stdlib.h>',tag_other,0)+ standardizeStr('int main(){',tag_body,0)
-END = standardizeStr('return 0;',tag_body,1)+standardizeStr('}',tag_other,0)
+END = standardizeStr('return 0;',tag_body,1)+'}'+49*' '+tag_body
 
 class Generator:
     def __init__(self):
@@ -33,6 +35,9 @@ class Generator:
 
     def flushSet(self):
         self.undeclare_set.clear()
+        self.declared_set.clear()
+        self.assigned_set.clear()
+        self.__table.clear()
         for i in range(MAX_OBJECT_NUM):
             self.undeclare_set.add('obj_'+str(i))
 
@@ -46,33 +51,34 @@ class Generator:
         return content,tag
 
     def assignNum(self,name):
-        tag = tag_safe
+
         value = random.randint(-MAX_VALUE, MAX_VALUE)
         symbol = self.__table.pop(name)
         if value <= 0 and symbol.getType() == TYPE_UINT:
             tag = tag_unsafe
             symbol.setValue(OVERFLOW_NUM+value)
         else:
+            tag = tag_safe
             symbol.setValue(value)
         self.__table.add(symbol)
         content = name+' = '+ str(value)+';'
-
         return content,tag
 
     def assignValue(self,lName,rName):
         lSymbol = self.__table.pop(lName)
         rSymbol = self.__table.find(rName)
         rValue = rSymbol.getValue()
-        rType = rSymbol.getType()
+        rType =rSymbol.getType()
         lType = lSymbol.getType()
-        tag = tag_safe
-        if lType==TYPE_UINT and rType ==TYPE_INT:
+
+        if lType==TYPE_UINT and rType == TYPE_INT:
             tag = tag_unsafe
-            if rValue <=0:
+            if rValue<=0:
                 lSymbol.setValue(OVERFLOW_NUM+rValue)
             else:
                 lSymbol.setValue(rValue)
         else:
+            tag = tag_safe
             lSymbol.setValue(rValue)
         self.__table.add(lSymbol)
         content = lName+' = '+rName+';'
@@ -110,8 +116,11 @@ class Generator:
                 if len(self.assigned_set)<2:
                     i=i+1
                     continue
-                slice = random.sample(self.assigned_set,2)
-                content,tag = func(slice[0],slice[1])
+                lValue = self.assigned_set.pop()
+                rValue = self.assigned_set.pop()
+                self.assigned_set.add(lValue)
+                self.assigned_set.add(rValue)
+                content,tag = func(lValue,rValue)
                 code += standardizeStr(content, tag,1)
                 # print(content,tag)
         return code
@@ -119,26 +128,112 @@ class Generator:
     def nomal_stm(self):
         self.flushSet()
         code = self.stm()
-        code+=END
-        print(code)
+        code += END
 
-    def if_stm(self):
+        # print(code)
+        return code
+
+    def while_stm(self):
         self.flushSet()
         code = self.stm(3, 4, 3)
         left_condition = self.assigned_set.pop()
+        self.assigned_set.add(left_condition)
+
+        left_condition_symbol = self.__table.pop(left_condition)
+        left_value = left_condition_symbol.getValue()
+        left_type = left_condition_symbol.getType()
+        right_value = random.randint(-MAX_VALUE,MAX_VALUE)
+
+        while_code = 'while(%s > %s){'%(left_condition,right_value)
+        state_code = '%s--;'%left_condition
+        while_end_code = '}'
+
+        state_tag = tag_safe
+
+        if left_value <= right_value:
+            state_tag =tag_body
+        else:
+            if left_type == TYPE_UINT and right_value <= 0:
+                state_tag = tag_unsafe
+                left_condition_symbol.setValue(OVERFLOW_NUM + right_value)
+            else:
+                left_condition_symbol.setValue(right_value)
+
+
+        self.__table.add(left_condition_symbol)
+        code+=standardizeStr(while_code,tag_body,1)
+        code+=standardizeStr(state_code,state_tag,2)
+        code+=standardizeStr(while_end_code,tag_body,1)
+        code += END
+
+        # print(code)
+        return code
+
+
+
+
+    def if_stm(self):
+        self.flushSet()
+        code = self.stm(5, 7, 4)
+        left_condition = self.assigned_set.pop()
         right_condition = self.assigned_set.pop()
         state = self.assigned_set.pop()
+        else_state = self.assigned_set.pop()
         self.assigned_set.add(left_condition)
         self.assigned_set.add(right_condition)
         self.assigned_set.add(state)
+        self.assigned_set.add(else_state)
+
+        change_num = random.randint(10, 40)
 
         if_code = 'if(%s < %s){' %(left_condition,right_condition)
-        state_code = '%s--;' % state
+        state_code = '%s -= %s;' % (state, change_num)
         else_code = '} else{'
-        else_state_code = '%s++;' %state
+        else_state_code = '%s = %s;' % (state, else_state)
         if_end_code = '}'
 
+        left_condition_symbol = self.__table.find(left_condition)
+        right_condition_symbol = self.__table.find(right_condition)
+        state_symbol = self.__table.pop(state)
+        else_state_symbol = self.__table.pop(else_state)
+        else_state_value = else_state_symbol.getValue()
+        state_type = state_symbol.getType()
+        state_value = state_symbol.getValue()
 
+        if_state_tag = tag_safe
+        else_state_tag = tag_safe
+
+        if left_condition_symbol.getValue() < right_condition_symbol.getValue():
+            else_state_tag =tag_body
+            state_value -= change_num
+            if state_value<=0 and state_type == TYPE_UINT:
+                if_state_tag = tag_unsafe
+                state_symbol.setValue(state_value+OVERFLOW_NUM)
+            else:
+                state_symbol.setValue(state_value)
+        else:
+            if_state_tag = tag_body
+            if else_state_symbol.getType() == TYPE_UINT and state_type == TYPE_INT:
+                else_state_tag = tag_unsafe
+                if else_state_value<= 0:
+                    else_state_value += OVERFLOW_NUM
+                    state_symbol.setValue(else_state_value)
+                else:
+                    state_symbol.setValue(else_state_value)
+            else:
+                state_symbol.setValue(else_state_value)
+
+        self.__table.add(state_symbol)
+        self.__table.add(else_state_symbol)
+
+        code+=standardizeStr(if_code,tag_body,1)
+        code+= standardizeStr(state_code,if_state_tag,2)
+        code+= standardizeStr(else_code, tag_body,1)
+        code += standardizeStr(else_state_code, else_state_tag,2)
+        code+= standardizeStr(if_end_code, tag_body,1)
+        code += END
+        # print(code)
+        return code
 
 
     def for_stm(self):
@@ -149,8 +244,8 @@ class Generator:
         condition = self.assigned_set.pop()
         state = self.assigned_set.pop()
         self.assigned_set.add(init)
-        self.assigned_set.add(condition)
         self.assigned_set.add(state)
+        self.assigned_set.add(condition)
 
         condition_symbol =  self.__table.find(condition)
         init_symbol = self.__table.pop(init)
@@ -165,48 +260,62 @@ class Generator:
         for_start_code = 'for(%s = %s; %s < %s; %s++){' %(init,init_value,init,condition,init)
         for_state_code = '%s--;' % state
         for_end_code = '}'
-        if init_symbol.getType()==TYPE_UINT and init_value<=0:
-            init_symbol.setValue(MAX_VALUE+init_value)
-
-            code+=standardizeStr(for_start_code,tag_unsafe,1)
-            code+=standardizeStr(for_state_code, tag_body,2)
-            code+=standardizeStr(for_end_code,tag_body,1)
-            code+=END
-
-            self.__table.add(init_symbol)
-            self.__table.add(state_symbol)
-            print(code)
-            return
-        new_state_value = state_value - differ_value
+        for_tag = tag_safe
         state_tag = tag_safe
-        if state_symbol.getType() == TYPE_UINT and new_state_value<0:
-            new_state_value+=MAX_VALUE
-            state_tag = tag_unsafe
-        state_symbol.setValue(new_state_value)
-        init_symbol.setValue(compare_value)
 
-        code += standardizeStr(for_start_code, tag_body,1)
-        code += standardizeStr(for_state_code, state_tag,2)
-        code += standardizeStr(for_end_code, tag_body,1)
-        code += END
+        # for赋值语句unsafe
+        if init_symbol.getType()==TYPE_UINT and init_value<=0:
+            for_tag = tag_unsafe
+            init_value += OVERFLOW_NUM
+            differ_value = compare_value - init_value
+
+        # for循环条件完结
+        if differ_value<=0:
+            state_tag = tag_body
+            compare_value = init_value
+        # 执行了for循环
+        else:
+            state_value -= differ_value
+            if state_symbol.getType() == TYPE_UINT and state_value <= 0:
+                state_value += OVERFLOW_NUM
+                state_tag = tag_unsafe
+        state_symbol.setValue(state_value)
+        init_symbol.setValue(compare_value)
 
         self.__table.add(init_symbol)
         self.__table.add(state_symbol)
-        print(code)
-
-        # print('table:')
-        # for i in self.__table.SymbolList:
-        #     print(i.getName(),i.getType(),i.getValue())
-
-
-
+        code += standardizeStr(for_start_code, for_tag,1)
+        code += standardizeStr(for_state_code, state_tag,2)
+        code += standardizeStr(for_end_code, tag_body,1)
+        code += END
+        # print(code)
+        return code
 
 
+    def print_value_table(self):
+        if not self.__table.SymbolList:
+            print('no value')
+        else:
+            for i in self.__table.SymbolList:
+                print('type:', i.getType(), 'name:',i.getName(), 'value:',i.getValue())
 
+    def getData(self, fileName, num):
+        if not os.path.exists(fileName):
+            print('wrong file path')
+            return
+        func_list = [self.nomal_stm, self.while_stm, self.if_stm, self.for_stm]
+        for i in range(num):
+            codePath = fileName+'/code_'+ str(i)+'.c'
+            func = func_list[i% len(func_list)]
+            with open(codePath,'w') as fileStream:
+                fileStream.write(func())
 
 
 
 
 if __name__ == '__main__':
     ge = Generator()
-    ge.nomal_stm()
+    ge.getData('D:/python_program/test/',10)
+    # for i in range(10):
+    #     print(ge.nomal_stm())
+    #     ge.print_value_table()
